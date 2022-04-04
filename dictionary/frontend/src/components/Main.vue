9391f09de277137b5b3e5d965a3ebb6ef1ad4356
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-04 14:29:32 +0300

    Add Vue.js

---
+++ dictionary/frontend/src/components/Main.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		{{ content }}
+	</div>
+</template>
+
+<script>
+import axios from 'axios'
+
+ export default {
+	 data() {
+		 return {
+			 content: ''
+		 }
+	 },
+	 created() {
+		 axios.get(`http://localhost:8080/`)
+		 .then(response => {
+			 console.log(response.data)
+      	this.content = response.data
+    })
+	 }
+ }
+</script>
+
+<style>
+
+</style>
--- dictionary/src/main/java/com/annoing/dictionary/controller/MainController.java
+++ dictionary/src/main/java/com/annoing/dictionary/controller/MainController.java
@@ -1,9 +1,11 @@
 package com.annoing.dictionary.controller;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin("http://localhost:8081/")
 public class MainController {
 
 	@GetMapping
