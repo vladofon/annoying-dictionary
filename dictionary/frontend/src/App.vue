a5c9bc866bd76c8ce54713d366e58e91f1c56316
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-05 19:24:23 +0300

    Create app-footer component

--- dictionary/frontend/src/App.vue
+++ dictionary/frontend/src/App.vue
@@ -1,19 +1,22 @@
 <template>
-	<v-app> 
+	<v-app class="d-flex"> 
 		<app-bar /> 
 		<v-main> 
-		
+
 		</v-main> 
+		<app-footer />
 	</v-app>
 </template>
 
 <script>
 import AppBar from '@/components/AppBar.vue'
+import AppFooter from '@/components/AppFooter.vue'
 
 export default {
   name: 'App',
   components: {
 		AppBar,
+		AppFooter,
   },
 }
 </script>
---
+++ dictionary/frontend/src/components/AppFooter.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-footer class="bg-teal-darken-2">
+    <v-card
+      elevation="0"
+      rounded="0"
+      width="100%"
+      class="bg-teal-darken-2 text-center">
+    
+      <v-card-text class="text-white">
+        {{ new Date().getFullYear() }} - <strong>Annoying dictionary</strong>
+      </v-card-text>
+      
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
