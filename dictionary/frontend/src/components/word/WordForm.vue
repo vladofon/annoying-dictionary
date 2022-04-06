1c6cacc796b78a8ea62a651dee5b5c84a4448483
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-06 13:56:57 +0300

    Create word-form component

---
+++ dictionary/frontend/src/components/word/WordForm.vue
@@ -0,0 +1,26 @@
+<template>
+
+	<v-container fluid class="bg-teal ">
+		<v-row>
+			<v-col class="d-flex">
+				<h3 class="text-white" style="margin: auto 0">Words set</h3>
+				<v-spacer></v-spacer>
+				<div>
+					<v-btn rounded="pill" class="text-black">
+						<v-icon color="black">mdi-plus</v-icon>
+						Add new
+					</v-btn>
+				</div>
+			</v-col>
+		</v-row>
+		
+	</v-container>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
