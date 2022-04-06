4e3d95f52edc9286a0657ba3a057686cc5b75ecf
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-06 13:58:12 +0300

    Add vue-router

--- dictionary/frontend/src/main.js
+++ dictionary/frontend/src/main.js
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify'
+import router from '@/router/index'
 import { loadFonts } from './plugins/webfontloader'
 
 loadFonts()
 
 createApp(App)
+	.use(router)
   .use(vuetify)
   .mount('#app')
