9b6aaceeaabf9ca0cd05955fd3e1689eff2b6bde
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-06 12:02:54 +0300

    Add google oauth

--- dictionary/src/main/java/com/annoing/dictionary/config/WebSecurityConfig.java
+++ dictionary/src/main/java/com/annoing/dictionary/config/WebSecurityConfig.java
@@ -1,20 +1,49 @@
 package com.annoing.dictionary.config;
 
+import java.time.LocalDateTime;
+
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.PrincipalExtractor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.annoing.dictionary.domain.User;
+import com.annoing.dictionary.repo.UserDetailsRepo;
 
 @Configuration
-public class WebSecurityConfig {
+@EnableWebSecurity
+@EnableOAuth2Sso
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests().mvcMatchers("/").permitAll().anyRequest().authenticated().and().csrf().disable();
+	}
+
 	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE",
-						"OPTIONS");
-			}
+	public PrincipalExtractor principalExtractor(UserDetailsRepo userDetailsRepo) {
+		return map -> {
+			String id = (String) map.get("sub");
+
+			User user = userDetailsRepo.findById(id).orElseGet(() -> {
+				User newUser = new User();
+
+				newUser.setId(id);
+				newUser.setName((String) map.get("name"));
+				newUser.setEmail((String) map.get("email"));
+				newUser.setLocale((String) map.get("locale"));
+				newUser.setGender((String) map.get("gender"));
+				newUser.setUserpic((String) map.get("picture"));
+
+				return newUser;
+			});
+
+			user.setLastVisit(LocalDateTime.now());
+
+			return userDetailsRepo.save(user);
 		};
 	}
 }
