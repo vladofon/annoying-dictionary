3329b19f5309277b3d51becf92315672be2700b2
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-06 13:56:16 +0300

    Create word-card component

---
+++ dictionary/frontend/src/components/word/WordCard.vue
@@ -0,0 +1,31 @@
+<template>
+	<v-card class="mt-5"
+		elevation="5">
+		
+    <v-row>
+			<v-col>
+				<v-card-title>
+					Title
+					<v-btn flat class="ml-1" icon="mdi-volume-high"></v-btn>
+					<v-spacer></v-spacer>
+					<v-btn flat icon="mdi-dots-vertical"></v-btn>
+				</v-card-title>
+				<v-card-subtitle>
+					Subtitle
+				</v-card-subtitle>
+				<v-card-text>
+					Some long and interesting context for a word that can be usefull for you
+				</v-card-text>
+			</v-col>
+    </v-row>
+
+	</v-card>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
