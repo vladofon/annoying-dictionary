0703325613b8e2eb3a18825000b7cb728f41289c
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-07 18:02:58 +0300

    Create set-dialog component

---
+++ dictionary/frontend/src/components/set/SetDialog.vue
@@ -0,0 +1,34 @@
+<template>
+	<app-dialog>
+		<template v-slot:dialog-title>
+			<span class="text-white">Create new set</span>
+		</template>
+		<v-text-field autofocus label="Name" variant="filled" color="teal"></v-text-field>
+		<v-textarea
+			rounded
+			color="teal"
+			class="bg-grey-lighten-3 pa-3 px-6 mt-n5"
+			variant="plain"
+			name="input-7-4"
+			label="Description"
+			placeholder="Enter some description..."
+		></v-textarea>
+		<template v-slot:dialog-actions>
+			create
+		</template>
+	</app-dialog>
+</template>
+
+<script>
+	import AppDialog from '@/components/AppDialog.vue'
+	
+	export default {
+		components: {
+			AppDialog
+		}
+	}
+</script>
+
+<style>
+
+</style>
