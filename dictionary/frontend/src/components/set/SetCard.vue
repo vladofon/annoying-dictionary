698f569f59790d06083d2fd44df8b914c6efd95b
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-07 18:02:10 +0300

    Create set-card component

---
+++ dictionary/frontend/src/components/set/SetCard.vue
@@ -0,0 +1,44 @@
+<template>
+  <v-card
+    class="ma-3"
+    width="344"
+    outlined
+  >
+    <v-list-item three-line>
+      <v-list-item-content>
+        <div class="text-overline mb-4">
+          PUBLIC SET
+        </div>
+        <v-list-item-title class="text-h5 mb-1">
+          Headline 5
+        </v-list-item-title>
+        <v-list-item-subtitle>Greyhound divisely hello coldly fonwderfully</v-list-item-subtitle>
+      </v-list-item-content>
+
+      <v-list-item-avatar
+        tile
+        size="80"
+        color="grey"
+      ></v-list-item-avatar>
+    </v-list-item>
+
+    <v-card-actions>
+      <v-btn
+        outlined
+        rounded
+        text
+        color="teal"
+      >
+        Show
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
