e8405635d1667906cf19fc03980128f91cd301f1
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-07 18:03:19 +0300

    Create set-form component

---
+++ dictionary/frontend/src/components/set/SetForm.vue
@@ -0,0 +1,34 @@
+<template>
+
+	<v-container fluid class="bg-teal ">
+		<v-row>
+			<v-col class="d-flex">
+				<h3 class="text-white" style="margin: auto 0">Your sets</h3>
+				<v-spacer></v-spacer>
+				<div>
+					<v-btn @click="switchDialog(true)" rounded="pill" class="text-black">
+						<v-icon color="black">mdi-plus</v-icon>
+						Create new
+					</v-btn>
+				</div>
+			</v-col>
+		</v-row>
+		
+	</v-container>
+</template>
+
+<script>
+import { mapMutations } from 'vuex'
+
+export default {
+		methods: {
+			...mapMutations({
+				switchDialog: 'switchDialog'
+			})
+		}
+	}
+</script>
+
+<style>
+
+</style>
