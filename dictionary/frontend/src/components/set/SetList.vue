3dc66b79f399134d4d177d187ef4fc5938bde3b3
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-07 18:02:31 +0300

    Create set-list component

---
+++ dictionary/frontend/src/components/set/SetList.vue
@@ -0,0 +1,19 @@
+<template>
+	<v-container>
+		<v-row>
+			<v-col cols="4" v-for="item in 15" :key="item">
+				<set-card/>
+			</v-col>
+		</v-row>
+	</v-container>
+</template>
+
+<script>
+	import SetCard from '@/components/set/SetCard.vue'
+	
+	export default {
+		components: {
+			SetCard
+		}
+	}
+</script>
