041e4203a9ef492746847510d819646503a20d81
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-08 18:23:18 +0300

    Create quiz page

---
+++ dictionary/frontend/src/router/QuizPage.vue
@@ -0,0 +1,14 @@
+<template>
+	<quiz-list /> 
+</template>
+
+<script>
+	import QuizList from '@/components/quiz/QuizList.vue'
+	
+	
+	export default {
+		components: {
+			QuizList,
+		}
+	}
+</script>
--- dictionary/frontend/src/router/index.js
+++ dictionary/frontend/src/router/index.js
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import WordsPage from '@/router/WordsPage.vue'
 import SetsPage from '@/router/SetsPage.vue'
+import QuizPage from '@/router/QuizPage.vue'
 
 const routes = [
   {path: '/words', component: WordsPage},
-  {path: '/sets', component: SetsPage}
+  {path: '/sets', component: SetsPage},
+  {path: '/quiz', component: QuizPage}
 ]
 
 const router = createRouter({
