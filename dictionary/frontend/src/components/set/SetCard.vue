e7f49f7b2a4162235d588195a8f695cab7bdcff5
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-09 16:30:37 +0300

    Add menu with options for set-item

--- dictionary/frontend/src/components/set/SetCard.vue
+++ dictionary/frontend/src/components/set/SetCard.vue
@@ -6,8 +6,8 @@
   >
     <v-list-item three-line>
       <v-list-item-content>
-        <div class="text-overline mb-4">
-          PUBLIC SET
+        <div class="text-overline mb-4 d-flex align-content-center">
+          <span class="align-self-center">PUBLIC SET</span>
         </div>
         <v-list-item-title class="text-h5 mb-1">
           Headline 5
@@ -20,6 +20,22 @@
         size="80"
         color="grey"
       ></v-list-item-avatar>
+      
+			<v-menu transition="scale-transition">
+				<template v-slot:activator="{ props }">
+					<v-btn v-bind="props" flat icon="mdi-dots-vertical" class="align-self-start"></v-btn>
+				</template>
+				
+				<v-list elevation="5" rounded class="pa-0">
+					<v-list-item>
+						<v-list-item-title><v-btn class="text-red" plain>Delete</v-btn></v-list-item-title>
+					</v-list-item>
+					<v-list-item>
+						<v-list-item-title><v-btn class="text-orange" plain>Private</v-btn></v-list-item-title>
+					</v-list-item>
+				</v-list>
+			</v-menu>
+      
     </v-list-item>
 
     <v-card-actions>
