f11cff30bfd0612c7cf3c0e55b70eadb4cae5189
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-11 17:13:40 +0300

    Add animations to items

--- dictionary/frontend/src/components/word/WordList.vue
+++ dictionary/frontend/src/components/word/WordList.vue
@@ -1,25 +1,78 @@
 <template>
 	<v-container class="pa-10">
-		<v-row>
-			<v-col>
-				<div v-for="word in 10" :key="word">
-					<word-card />
-				</div>
-			</v-col>
-		</v-row>
+		<transition name="switch" mode="out-in">
+			<v-row v-if="words.length">
+				<v-col>
+					<transition-group name="list" appear>
+						<div v-for="word in words" :key="word.id">
+							<word-card :word="word"/>
+						</div>
+					</transition-group>
+				</v-col>
+			</v-row>
+			<v-row v-else class="justify-center align-center">
+				<v-col class="align-self-center">
+					<h1 class="text-center" rounded>You have not any item in set!</h1>
+				</v-col>
+			</v-row>
+		</transition>
 	</v-container>
 </template>
 
 <script>
 	import WordCard from '@/components/word/WordCard.vue'
+	import { mapState } from 'vuex'
 	
 	export default {
 		components: {
 			WordCard
-		}	
+		},
+		computed: {
+			...mapState('word', ['words'])
+		}
 	}
 </script>
 
-<style>
-
+<style scoped>
+	.list-enter-active {
+		transition: all 0.4s ease;
+	}
+	.list-leave-active {
+		transition: all 0.4s ease;
+		position: absolute;
+	}
+	.list-enter-from {
+		opacity: 0;
+		transform: scale(0.6)
+	}
+	.list-enter-to {
+		opacity: 1;
+		transform: scale(1)
+	}
+	.list-leave-from {
+		opacity: 1;
+		transform: scale(1)
+	}
+	.list-leave-to {
+		opacity: 0;
+		transform: scale(0.6)
+	}
+	.list-move {
+		transition: all 0.3s ease;
+	}
+	
+	
+  .switch-enter-from,
+  .switch-leave-to {
+    opacity: 0;
+    transform: translateY(20px);
+  }
+  .switch-enter-active{
+    transition: all 0.5s ease;
+  }
+  .switch-leave-active {
+    transition: all 0.5s ease;
+    position: absolute;
+    width: 100%;
+  }
 </style>
