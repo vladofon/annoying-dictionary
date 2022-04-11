b755bb562921a7da47e3441745eeebf6eae43cc5
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-04-11 17:14:49 +0300

    Add iteam creation

--- dictionary/frontend/src/components/word/WordDialog.vue
+++ dictionary/frontend/src/components/word/WordDialog.vue
@@ -3,8 +3,9 @@
 		<template v-slot:dialog-title>
 			<span class="text-white">Add new item to set</span>
 		</template>
-		<v-text-field autofocus label="Value" variant="filled" color="teal"></v-text-field>
+		<v-text-field v-model="word.value" autofocus label="Value" variant="filled" color="teal"></v-text-field>
 		<v-textarea
+			v-model="word.context"
 			rounded
 			color="teal"
 			class="bg-grey-lighten-3 pa-3 px-6 mt-n5"
@@ -14,17 +15,42 @@
 			placeholder="Try to describe this word or phrase in a few sentences..."
 		></v-textarea>
 		<template v-slot:dialog-actions>
-			add item
+			<v-btn @click="createWord" color="teal" text>
+				Add
+			</v-btn>
 		</template>
 	</app-dialog>
 </template>
 
 <script>
 	import AppDialog from '@/components/AppDialog.vue'
+	import { mapMutations } from 'vuex'
 	
 	export default {
 		components: {
 			AppDialog
+		},
+		data() {
+			return {
+				word: {
+					value: '',
+					context: ''
+				}
+			}
+		},
+		methods: {
+			...mapMutations('word', ['addWord']),
+			...mapMutations(['switchDialog']),
+			createWord() {
+				this.word.id = Math.floor(Math.random() * 100000)
+				this.addWord(this.word)
+				this.switchDialog(false)
+				
+				this.word = {
+					value: '',
+					context: ''
+				}
+			}
 		}
 	}
 </script>
