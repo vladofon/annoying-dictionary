e445caba6f95e5bc40530902399e0510ed69f63a
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-07 19:55:18 +0300

    Add method for word creation

--- dictionary/frontend/src/components/word/WordForm.vue
+++ dictionary/frontend/src/components/word/WordForm.vue
@@ -6,7 +6,7 @@
 				<h3 class="text-white" style="margin: auto 0">Words set</h3>
 				<v-spacer></v-spacer>
 				<div>
-					<v-btn @click="switchDialog(true)" rounded="pill" class="text-black">
+					<v-btn @click="createWord" rounded="pill" class="text-black">
 						<v-icon color="black">mdi-plus</v-icon>
 						Add new
 					</v-btn>
@@ -19,14 +19,22 @@
 
 <script>
 import { mapMutations } from 'vuex'
+import DialogModes from '@/components/word/misc/DialogModes'
 
 export default {
-		methods: {
-			...mapMutations({
-				switchDialog: 'switchDialog'
-			})
+	methods: {
+		...mapMutations({
+			switchDialog: 'switchDialog'
+		}),
+		...mapMutations('word', ['setDialogMode', 'setOperableWord']),
+		
+		createWord() {
+			this.setDialogMode(DialogModes.CREATE)
+			this.setOperableWord({id:null, value:'', context:''})
+			this.switchDialog(true)
 		}
 	}
+}
 </script>
 
 <style>
