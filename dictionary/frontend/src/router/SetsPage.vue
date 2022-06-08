ff8799f21515d0d119f704773156f978c42935d4
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-08 15:13:39 +0300

    Update sets-page

--- dictionary/frontend/src/router/SetsPage.vue
+++ dictionary/frontend/src/router/SetsPage.vue
@@ -1,22 +1,19 @@
 <template>
 	<set-form />
-	<!-- <set-dialog /> -->
-	<set-edit-dialog />
+	<set-dialog />
 	<set-list/>
 </template>
 
 <script>
 	import SetList from '@/components/set/SetList.vue'
 	import SetForm from '@/components/set/SetForm.vue'
-	//import SetDialog from '@/components/set/SetDialog.vue'
-	import SetEditDialog from '@/components/set/SetEditDialog.vue'
+	import SetDialog from '@/components/set/SetDialog.vue'
 	
 	export default {
 		components: {
 			SetList,
 			SetForm,
-			//SetDialog,
-			SetEditDialog
+			SetDialog
 		}
 	}
 </script>
