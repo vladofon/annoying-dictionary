5bcba2fd63129c5e3bb30fe98a7a1eadfe448c39
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-09 19:00:29 +0300

    Create app-menu component

---
+++ dictionary/frontend/src/components/AppMenu.vue
@@ -0,0 +1,23 @@
+<template>
+	<v-menu v-model="menu" transition="scale-transition" :close-on-content-click="true">
+		<template v-slot:activator="{ props }">
+			<v-btn v-bind="props" flat icon="mdi-dots-vertical" class="align-self-start"></v-btn>
+		</template>
+		
+		<v-list elevation="5" rounded class="pa-0" @click="menu = false">
+		
+			<slot></slot>
+	
+		</v-list>
+	</v-menu>
+</template>
+
+<script>
+	export default {
+		data() {
+			return {
+				menu: false
+			}
+		}
+	}
+</script>
