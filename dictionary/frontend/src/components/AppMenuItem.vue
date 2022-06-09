17774ea5f1b8df82f07f5816aad0a8386e72861e
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-09 19:00:43 +0300

    Create app-menu-item component

---
+++ dictionary/frontend/src/components/AppMenuItem.vue
@@ -0,0 +1,13 @@
+<template>
+	<v-list-item>
+		<v-list-item-title>
+			<v-btn @click="$emit('action')" :class="`text-${color}`" plain>{{text}}</v-btn>
+		</v-list-item-title>
+	</v-list-item>
+</template>
+
+<script>
+	export default {
+		props: ['color', 'text']
+	}
+</script>
