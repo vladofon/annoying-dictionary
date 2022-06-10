84ec160074cfa5ab3b72a63ff52ce183b83cad1f
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-10 21:24:54 +0300

    Create profile page

--- dictionary/frontend/src/router/index.js
+++ dictionary/frontend/src/router/index.js
@@ -3,12 +3,14 @@ import WordsPage from '@/router/WordsPage.vue'
 import SetsPage from '@/router/SetsPage.vue'
 import QuizPage from '@/router/QuizPage.vue'
 import SetPage from '@/router/SetPage.vue'
+import ProfilePage from '@/router/ProfilePage.vue'
 
 const routes = [
   {path: '/words', component: WordsPage},
   {path: '/sets', component: SetsPage},
   {path: '/sets/:id', component: SetPage},
   {path: '/quiz', component: QuizPage},
+  {path: '/profile', component: ProfilePage},
 ]
 
 const router = createRouter({
