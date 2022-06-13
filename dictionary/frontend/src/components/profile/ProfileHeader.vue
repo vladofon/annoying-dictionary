62c6a5317c311f659bbc102c5e7ecce6c0b42f28
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-13 20:12:04 +0300

    Create profile card

---
+++ dictionary/frontend/src/components/profile/ProfileHeader.vue
@@ -0,0 +1,54 @@
+<template>
+	<v-container class="my-3">
+		<v-card class="mx-auto" max-width="634" tile>
+			<v-img height="100%" cover src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg" class="pa-3">
+				<v-row>
+					<v-col cols="6">
+						
+							<v-avatar color="grey" size="150" rounded="10" >
+								<v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg" height="100%" cover></v-img>
+							</v-avatar>
+							<v-list-item class="text-white" title="Marcus Obrien" subtitle="Free account">
+							
+							</v-list-item>
+	
+					</v-col>
+					<v-col cols="6">
+						
+							<v-card class="text-center ma-3">
+								<h2 class="text-center">
+									Sets
+								</h2>
+								<div>
+									15
+								</div>
+							</v-card>
+							
+							<v-card class="text-center ma-3">
+								<h2 class="text-center">
+									Words
+								</h2>
+								<div>
+									360
+								</div>
+							</v-card>
+							
+							<v-card class="text-center ma-3">
+								<h2 class="text-center">
+									Featured
+								</h2>
+								<div>
+									10
+								</div>
+							</v-card>
+						
+					</v-col>
+				</v-row>
+			</v-img>
+		</v-card>
+	</v-container>
+</template>
+
+<script>
+
+</script>
---
+++ dictionary/frontend/src/router/ProfilePage.vue
@@ -0,0 +1,13 @@
+<template>
+	<profile-header/>
+</template>
+
+<script>
+	import ProfileHeader from "@/components/profile/ProfileHeader.vue"
+	
+	export default {
+		components: {
+			ProfileHeader
+		}
+	}
+</script>
