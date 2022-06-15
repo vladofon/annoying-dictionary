9d416ae0ecee84b286e8fab1bee25982451760d4
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-15 20:46:31 +0300

    Add set list to profile page

--- dictionary/frontend/src/router/ProfilePage.vue
+++ dictionary/frontend/src/router/ProfilePage.vue
@@ -1,14 +1,26 @@
 <template>
+	<profile-form />
 	<profile-header/>
+	
+	<v-container fluid class="bg-teal ">
+		<v-row>
+			<v-col class="d-flex justify-center">
+				<h3 class="text-white text-center">Popular sets</h3>
+			</v-col>
+		</v-row>
+	</v-container>
+	
 	<set-list/>
 </template>
 
 <script>
 	import ProfileHeader from "@/components/profile/ProfileHeader.vue"
-	import SetList from "@/components/profile/SetList.vue"
+	import ProfileForm from "@/components/profile/ProfileForm.vue"
+	import SetList from "@/components/set/SetList.vue"
 	
 	export default {
 		components: {
+			ProfileForm,
 			ProfileHeader,
 			SetList
 		}
