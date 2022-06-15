5c7eb045a6633f2fb980b5729f3f40e0e24c7ca0
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-15 20:44:09 +0300

    Redesign profile header cards

--- dictionary/frontend/src/components/profile/ProfileHeader.vue
+++ dictionary/frontend/src/components/profile/ProfileHeader.vue
@@ -3,7 +3,7 @@
 		<v-card class="mx-auto" max-width="634" tile>
 			<v-img height="100%" cover src="https://cdn.vuetifyjs.com/images/cards/server-room.jpg" class="pa-3">
 				<v-row>
-					<v-col cols="6">
+					<v-col cols="5">
 						
 							<v-avatar color="grey" size="150" rounded="10" >
 								<v-img src="https://cdn.vuetifyjs.com/images/profiles/marcus.jpg" height="100%" cover></v-img>
@@ -13,35 +13,48 @@
 							</v-list-item>
 	
 					</v-col>
-					<v-col cols="6">
-						
-							<v-card class="text-center ma-3">
-								<h2 class="text-center">
-									Sets
-								</h2>
-								<div>
-									15
-								</div>
-							</v-card>
+					<v-col cols="7">
+					
+						<v-container>
+							<v-row>
+								<v-col cols="6">
+									<v-card class="text-center pa-3">
+										<h4 class="text-center pa-1 bg-teal">
+											<span class="text-white">Sets</span>
+										</h4>
+										<v-divider></v-divider>
+										<div class="pa-3 pt-4">
+											15
+										</div>
+									</v-card>
+								</v-col>
+								
+								<v-col cols="6">
+									<v-card class="text-center pa-3">
+										<h4 class="text-center pa-1 bg-teal">
+											<span class="text-white">Words</span>
+										</h4>
+										<v-divider></v-divider>
+										<div class="pa-3 pt-4">
+											360
+										</div>
+									</v-card>
+								</v-col>
+								
+								<v-col cols="6">
+									<v-card class="text-center pa-3">
+										<h4 class="text-center pa-1 bg-teal">
+											<span class="text-white">Featured</span>
+										</h4>
+										<v-divider></v-divider>
+										<div class="pa-3 pt-4">
+											10
+										</div>
+									</v-card>
+								</v-col>
+							</v-row>
+						</v-container>
 							
-							<v-card class="text-center ma-3">
-								<h2 class="text-center">
-									Words
-								</h2>
-								<div>
-									360
-								</div>
-							</v-card>
-							
-							<v-card class="text-center ma-3">
-								<h2 class="text-center">
-									Featured
-								</h2>
-								<div>
-									10
-								</div>
-							</v-card>
-						
 					</v-col>
 				</v-row>
 			</v-img>
