2aed23190f984c0214b602359fee77c897beb5ad
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-15 20:43:46 +0300

    Add styles to profile form

---
+++ dictionary/frontend/src/components/profile/ProfileForm.vue
@@ -0,0 +1,18 @@
+<template>
+
+	<v-container fluid class="bg-teal ">
+		<v-row>
+			<v-col class="d-flex">
+				<h3 class="text-white" style="margin: auto 0">Profile</h3>
+				<v-spacer></v-spacer>
+				<div>
+					<v-btn rounded="pill" class="text-black">
+						<v-icon color="black">mdi-plus</v-icon>
+						Change info
+					</v-btn>
+				</div>
+			</v-col>
+		</v-row>
+		
+	</v-container>
+</template>
