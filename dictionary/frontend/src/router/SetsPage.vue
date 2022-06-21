8dd09df4a0d3e6b2aec40bff7ba3c935d05e3539
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-21 19:08:25 +0300

    Init default set-list

--- dictionary/frontend/src/router/SetsPage.vue
+++ dictionary/frontend/src/router/SetsPage.vue
@@ -1,19 +1,23 @@
 <template>
 	<set-form />
 	<set-dialog />
-	<set-list/>
+	<set-list :sets="sets"/>
 </template>
 
 <script>
 	import SetList from '@/components/set/SetList.vue'
 	import SetForm from '@/components/set/SetForm.vue'
-	import SetDialog from '@/components/set/SetDialog.vue'
+	import SetDialog from '@/components/set/SetDialog.vue' 
+	import { mapState } from 'vuex'
 	
 	export default {
 		components: {
 			SetList,
 			SetForm,
 			SetDialog
+		},
+		methods: {
+			...mapState('set', ['sets'])
 		}
 	}
 </script>
