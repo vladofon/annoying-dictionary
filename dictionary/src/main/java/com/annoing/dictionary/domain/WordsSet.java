f51afa9009d09674f559f9f99be312a4d709537d
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-27 19:32:02 +0300

    Create words-set entity

--- dictionary/src/main/java/com/annoing/dictionary/domain/WordsSet.java
+++ dictionary/src/main/java/com/annoing/dictionary/domain/WordsSet.java
@@ -1,30 +1,29 @@
 package com.annoing.dictionary.domain;
 
+import java.util.Set;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 
 @Entity
-public class Word {
+public class WordsSet {
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 
-	private String value;
+	@Column(length = 100)
+	private String name;
 
 	@Column(length = 1000)
-	private String context;
+	private String description;
 
-	@ManyToOne
-	@JoinColumn(name = "set_id", nullable = false)
-	private WordsSet set;
-
-	public Word() {
-	}
+	@OneToMany(mappedBy = "set")
+	private Set<Word> words;
 
 	public Long getId() {
 		return id;
@@ -34,33 +33,28 @@ public class Word {
 		this.id = id;
 	}
 
-	public String getValue() {
-		return value;
+	public String getName() {
+		return name;
 	}
 
-	public void setValue(String value) {
-		this.value = value;
+	public void setName(String name) {
+		this.name = name;
 	}
 
-	public String getContext() {
-		return context;
+	public String getDescription() {
+		return description;
 	}
 
-	public void setContext(String context) {
-		this.context = context;
+	public void setDescription(String description) {
+		this.description = description;
 	}
 
-	public WordsSet getSet() {
-		return set;
+	public Set<Word> getWords() {
+		return words;
 	}
 
-	public void setSet(WordsSet set) {
-		this.set = set;
-	}
-
-	@Override
-	public String toString() {
-		return "Word [id=" + id + ", value=" + value + "]";
+	public void setWords(Set<Word> words) {
+		this.words = words;
 	}
 
 	@Override
@@ -79,7 +73,7 @@ public class Word {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Word other = (Word) obj;
+		WordsSet other = (WordsSet) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
