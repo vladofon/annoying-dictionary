d4bb634bc912fa7e41d6917ed03cd7602e35ae42
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-06-29 19:41:26 +0300

    Fix paths and typo

--- dictionary/frontend/src/api/word.js
+++ dictionary/frontend/src/api/word.js
@@ -1,9 +1,9 @@
 import { api } from '@/api/api-resource'
 
 export default {
-	create: word => api.post('words/create', word),
-	edit: word => api.post('words/edit', word),
+	create: word => api.post('words', word),
+	edit: word => api.put(`words/${word.id}`, word),
 	list: () => api.get('words'),
 	get: id => api.get(`words/${id}`),
-	remove: id => api.delete(`words/${id}`)
+	delete: id => api.delete(`words/${id}`)
 }
