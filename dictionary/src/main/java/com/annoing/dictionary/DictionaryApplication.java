642930ce2cf04593aa36b8010acdf1e5ee7fecbd
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-07-04 11:33:07 +0300

    Fix error with login requests

--- dictionary/src/main/java/com/annoing/dictionary/DictionaryApplication.java
+++ dictionary/src/main/java/com/annoing/dictionary/DictionaryApplication.java
@@ -2,12 +2,19 @@ package com.annoing.dictionary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.context.request.RequestContextListener;
 
 @SpringBootApplication
 public class DictionaryApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DictionaryApplication.class, args);
+	}
+
+	@Bean
+	public RequestContextListener requestContextListener() {
+		return new RequestContextListener();
 	}
 
 }
