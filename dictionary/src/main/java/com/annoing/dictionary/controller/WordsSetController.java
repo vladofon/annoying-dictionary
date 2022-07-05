8b32c15adc945ec64b04241e66e4686129f3d5a7
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-07-05 14:12:47 +0300

    Create controller-method for user words-set

--- dictionary/src/main/java/com/annoing/dictionary/controller/WordsSetController.java
+++ dictionary/src/main/java/com/annoing/dictionary/controller/WordsSetController.java
@@ -13,8 +13,12 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.annoing.dictionary.domain.User;
 import com.annoing.dictionary.domain.WordsSet;
+import com.annoing.dictionary.domain.view.WordsSetView;
+import com.annoing.dictionary.service.UserService;
 import com.annoing.dictionary.service.WordsSetService;
+import com.fasterxml.jackson.annotation.JsonView;
 
 @CrossOrigin
 @RestController
@@ -22,18 +26,22 @@ import com.annoing.dictionary.service.WordsSetService;
 public class WordsSetController {
 
 	private WordsSetService wordsSetService;
+	private UserService userService;
 
 	@Autowired
-	WordsSetController(WordsSetService wordsSetService) {
+	WordsSetController(WordsSetService wordsSetService, UserService userService) {
 		this.wordsSetService = wordsSetService;
+		this.userService = userService;
 	}
 
 	@GetMapping
+	@JsonView(WordsSetView.QuickView.class)
 	public List<WordsSet> list() {
 		return wordsSetService.getAll();
 	}
 
 	@GetMapping("{id}")
+	@JsonView(WordsSetView.QuickView.class)
 	public WordsSet getOne(@PathVariable Long id) {
 		return wordsSetService.getOne(id);
 	}
@@ -51,5 +59,18 @@ public class WordsSetController {
 	@DeleteMapping("{id}")
 	public void delete(@PathVariable Long id) {
 		wordsSetService.remove(id);
+	}
+
+	@GetMapping("user/{id}")
+	@JsonView(WordsSetView.QuickView.class)
+	public List<WordsSet> userSets(@PathVariable String id) {
+		User user = userService.getUser(id);
+		List<WordsSet> sets = wordsSetService.userSets(user);
+
+		if (sets.isEmpty()) {
+			sets.add(wordsSetService.createDefaultSet(user));
+		}
+
+		return sets;
 	}
 }
