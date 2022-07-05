42252e6745bde91d219adae496110976e384febe
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-07-05 14:06:55 +0300

    Add credentials to request

--- dictionary/frontend/src/api/set.js
+++ dictionary/frontend/src/api/set.js
@@ -1,9 +1,10 @@
 import { api } from '@/api/api-resource'
 
 export default {
-	create: set => api.post('sets', set),
-	edit: set => api.put(`sets/${set.id}`, set),
-	list: () => api.get('sets'),
-	get: id => api.get(`sets/${id}`),
-	delete: id => api.delete(`sets/${id}`)
+	create: set => api.post('sets', set, {withCredentials: true}),
+	edit: set => api.put(`sets/${set.id}`, set, {withCredentials: true}),
+	list: () => api.get('sets', {withCredentials: true}),
+	listByUser: id => api.get(`sets/user/${id}`, {withCredentials: true}),
+	get: id => api.get(`sets/${id}`, {withCredentials: true}),
+	delete: id => api.delete(`sets/${id}`, {withCredentials: true})
 }
