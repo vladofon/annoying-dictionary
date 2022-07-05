81065ca4e6f032e791580ed5826920206da84d01
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-07-05 14:10:34 +0300

    Create repository for words-set domain

--- dictionary/src/main/java/com/annoing/dictionary/repo/WordsSetRepo.java
+++ dictionary/src/main/java/com/annoing/dictionary/repo/WordsSetRepo.java
@@ -1,11 +1,20 @@
 package com.annoing.dictionary.repo;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import com.annoing.dictionary.domain.User;
 import com.annoing.dictionary.domain.WordsSet;
 
 @Repository
 public interface WordsSetRepo extends JpaRepository<WordsSet, Long> {
 
+	@Query
+	List<WordsSet> findByDefaultSetTrue();
+
+	@Query
+	List<WordsSet> findByAuthor(User author);
 }
