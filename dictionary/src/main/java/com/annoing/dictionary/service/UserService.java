fcc9b2091fc589c51d0269001831f920cda40171
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-07-05 14:10:51 +0300

    Create service for user domain

--- dictionary/src/main/java/com/annoing/dictionary/service/UserService.java
+++ dictionary/src/main/java/com/annoing/dictionary/service/UserService.java
@@ -1,8 +1,22 @@
 package com.annoing.dictionary.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.annoing.dictionary.domain.User;
+import com.annoing.dictionary.repo.UserDetailsRepo;
 
 @Service
 public class UserService {
 
+	private final UserDetailsRepo userRepo;
+
+	@Autowired
+	public UserService(UserDetailsRepo userRepo) {
+		this.userRepo = userRepo;
+	}
+
+	public User getUser(String id) {
+		return userRepo.findById(id).get();
+	}
 }
