d2e14be0c4b44a1ffb421508d47680393da701e5
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-07-21 17:16:30 +0300

    Redesign profile card

--- dictionary/frontend/src/components/profile/ProfileCard.vue
+++ dictionary/frontend/src/components/profile/ProfileCard.vue
@@ -1,5 +1,5 @@
 <template>
-	<v-card color="#1F7087" theme="dark">
+	<v-card color="#1F7087" theme="dark" class="ma-1">
 		<div class="d-flex flex-no-wrap justify-space-between">
 		
 			<div>
@@ -7,11 +7,11 @@
 					{{profileData.name}}
 				</v-card-title>
 				
-				<v-card-subtitle>Foster the People</v-card-subtitle>
+				<v-card-subtitle><v-icon class="mr-1">mdi-heart</v-icon>{{profileData.likes}} likes</v-card-subtitle>
 				
 				<v-card-actions>
 					<v-btn class="ml-2" variant="outlined" size="small">
-						START RADIO
+						profile
 					</v-btn>
 				</v-card-actions>
 			</div>
@@ -23,16 +23,34 @@
 		</div>
 		
 		<v-card-actions>
-				<v-expansion-panels variant="inset" class="my-4">
+				<v-expansion-panels v-model="panel" variant="inset" class="my-4">
 					<v-expansion-panel
 						class="ma-1"
 						v-for="i in 3"
 						:key="i"
 						title="User set with words"
-						text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
-					></v-expansion-panel>
+					>
+						<v-expansion-panel-text>
+							text="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat."
+							<br/>
+							<v-btn variant="outlined" class="mt-3">Show</v-btn>
+						</v-expansion-panel-text>
+					</v-expansion-panel>
 				</v-expansion-panels>
+				
 			</v-card-actions>
+			
+			<v-container>
+				<v-row>
+					<v-col>
+						<v-btn>Show more</v-btn>
+					</v-col>
+					<v-col class="pa-4">
+						<v-icon>mdi-view-dashboard</v-icon>
+						{{profileData.sets}} sets
+					</v-col>
+				</v-row>
+			</v-container>
 			
 	</v-card>
 </template>
@@ -46,7 +64,8 @@ export default {
 				userpic: 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F13%2F2015%2F04%2F05%2Ffeatured.jpg&q=60',
 				likes: 2387,
 				sets: 10,
-			}
+			},
+			panel: [1 ,1 , 1],
 		}
 	}
 }
