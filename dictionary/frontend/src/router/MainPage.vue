df323603595c88b4646a9ba4fbb0524fa6846d9d
Alexander Vladovsky <alexvladovsky@ukr.net>
2022-10-12 20:08:34 +0300

    Add register link to main page

--- dictionary/frontend/src/router/MainPage.vue
+++ dictionary/frontend/src/router/MainPage.vue
@@ -1,3 +1,19 @@
 <template>
-	<!-- <a href="/login">Try to login</a> --><div>vue</div> 
+	<v-container>
+		<v-row>
+			<v-col>
+				<a @click="login"><h3>Register or login now!</h3></a>
+			</v-col>
+		</v-row>
+	</v-container>
 </template>
+
+<script>
+export default {
+	methods: {
+		login() {
+			window.location.href = process.env.VUE_APP_BACKEND_URL + "sessions/Google/callback";
+		},
+	}
+}
+</script>
